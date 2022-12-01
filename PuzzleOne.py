ea5151f5577681c1ad75b49a846c5b25d82be82f
Roger McClain <rogerhmcclain@gmail.com>
2022-12-01 14:19:45 -0800

    Add files via upload

---
+++ PuzzleOne.py
@@ -0,0 +1,21 @@
+
+with open('InputforPuzzleOne.txt') as f:
+
+    text = f.read()
+    myarray = text.split("\n\n")
+     
+    list_of_sums = []
+    for line in myarray:
+        lower_list = line.split("\n")
+        lower_list = list(map(int, lower_list))
+        sums = sum(lower_list)
+        list_of_sums.append(sums)
+
+    
+    num_max_cals_elf = max(list_of_sums)
+    print(f'Biggest calories: {num_max_cals_elf}')
+
+    sorted_sums_desc = sorted(list_of_sums, reverse=True)
+    topthree = sorted_sums_desc[:3]
+    sum_top_three = sum(topthree)
+    print(f'Sum of top three: {sum_top_three}')
